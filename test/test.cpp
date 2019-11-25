1aeaccd7620957684b07d4cc435154c553b7dc1f
Arjung27 <arjun.g1511@gmail.com>
2019-11-25 05:59:59 -0500

    [test/test.cpp] Modifying the name of the header file included

--- test/test.cpp
+++ test/test.cpp
@@ -9,7 +9,7 @@
  *               cite if code is used.
  */
 #include <gtest/gtest.h>
-#include "../include/pid_controller.hpp"
+#include "../include/pidController.hpp"
 
 /** 
  * @brief Test for setGainValues() function of the PidController class.
@@ -17,7 +17,7 @@
  * getGainValues() function are same or not.
  */
 TEST(checkValues, shouldPass) {
-  PidController pid;
+  pidController pid;
   pid.setGainValues(2, 3, 4);
   vector<float> inputValues = {2.0, 3.0, 4.0};
   int i = 0;
@@ -31,12 +31,12 @@ TEST(checkValues, shouldPass) {
     }
 }
 /** 
- * @brief Test for computePidError() function of the PidController class.
+ * @brief Test for computePidError() function of the pidController class.
  * It verifies that the computed value is equal to the expected value for 
  * control error less than the threshold value.
  */
 TEST(checkFormula, shouldPass) {
-  PidController pid;
+  pidController pid;
   pid.setGainValues(1, 2, 4);
   pid.setThreshold(10.0);
   pid.computePidError(5, 3, 2);
@@ -46,12 +46,12 @@ TEST(checkFormula, shouldPass) {
   ASSERT_EQ(6.0, pid.getControlError());
 }
 /** 
- * @brief Test for computePidError() function of the PidController class.
+ * @brief Test for computePidError() function of the pidController class.
  * It verifies that the computed value is equal to the expected value for 
  * control error more than the threshold value.
  */
 TEST(checkBoundaryCondition, shouldPass) {
-  PidController pid;
+  pidController pid;
   pid.setGainValues(1, 2, 4);
   pid.setThreshold(10.0);
   pid.computePidError(9, 4, 2);
